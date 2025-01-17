dfac20d232d1f0ad20913ead2a465bef75bef8dc
Jonathan Grangien <jonathan.grangien@consid.se>
2025-01-17 10:23:46 +0100

    Nvim: Allow range in order to set a specific theme

--- nvim/lua/theme_helpers.lua
+++ nvim/lua/theme_helpers.lua
@@ -1,52 +1,49 @@
 local theme_helpers = {}
 
-ThemeHelpersColorSchemeIndex = 0
+ThemeHelpersColorSchemeIndex = 1
 
 local function set_gruvbox_theme()
   vim.o.background = 'dark'
   vim.g['gruvbox_italic'] = 1
   vim.g['airline_theme'] = 'gruvbox'
   vim.cmd('colorscheme gruvbox')
-
-  vim.print('Gruvbox')
 end
 
 local function set_papercolor_theme()
   vim.o.background = 'light'
+  vim.g['airline_theme'] = 'papercolor'
   vim.cmd('colorscheme Papercolor')
   --let g:airline_theme='papercolor'
-  vim.g['airline_theme'] = 'papercolor'
-
-  vim.print('Papercolor light')
 end
 
 local function set_tokyonight_theme()
   vim.o.background = 'dark'
+  vim.g['airline_theme'] = 'gruvbox'
   vim.cmd('colorscheme tokyonight-night')
-
-  vim.print('Tokyonight')
 end
 
 local function set_tokyoday_theme()
   vim.o.background = 'light'
+  vim.g['airline_theme'] = 'papercolor'
   vim.cmd('colorscheme tokyonight-day')
-
-  vim.print('Tokyonight day')
 end
 
 function theme_helpers.flip_to_next_colorscheme()
-  ThemeHelpersColorSchemeIndex = ThemeHelpersColorSchemeIndex + 1
+  -- Set theme index explicitly if range was supplied, otherwise increment to next theme
+  ThemeHelpersColorSchemeIndex = (vim.v.count > 0) and vim.v.count or ThemeHelpersColorSchemeIndex + 1
 
-  if ThemeHelpersColorSchemeIndex == 1 then
+  if ThemeHelpersColorSchemeIndex == 2 then
     set_papercolor_theme()
-  elseif ThemeHelpersColorSchemeIndex == 2 then
-    set_tokyonight_theme()
   elseif ThemeHelpersColorSchemeIndex == 3 then
+    set_tokyonight_theme()
+  elseif ThemeHelpersColorSchemeIndex == 4 then
     set_tokyoday_theme()
   else
     set_gruvbox_theme()
-    ThemeHelpersColorSchemeIndex = 0
+    ThemeHelpersColorSchemeIndex = 1
   end
+
+  vim.print('1 Gruvbox | 2 Papercolor | 3 Tokyonight | 4 Tokyonight-day')
 end
 
 return theme_helpers
