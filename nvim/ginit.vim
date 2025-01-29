7de6cc68e5a836df07f93ee86df7eafc3d408ada
Jonathan Grangien <jonathan.grangien@gmail.com>
2025-01-29 14:31:32 +0100

    Purge old vim config

--- nvim/ginit.vim
+++
@@ -1,5 +0,0 @@
-" Loaded in GUIs like nvim-qt
-GuiPopupmenu 0 
-GuiTabline 0
-GuiFont! Meslo\ LG\ M\ for\ Powerline:h12
-
--- nvim/test.ts
+++
@@ -1,8 +0,0 @@
-export const testFunction = (str: string): string => {
-	if (str === 4) {
-
-		return str
-	}
-
-	return str + 'Hej';
-}
